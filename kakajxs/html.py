416badde26d2eb9a618e112f1e8c061dfc8f72eb
kakajxs <jiangxusheng@114-212-43-242.nju.edu.cn>
2014-05-28 19:38:50 +0800

    评论数

---
+++ kakajxs/html.py
@@ -0,0 +1,100 @@
+from data import get_commodity_data
+
+def indexpage0():
+	page='<!DOCTYPE HTML>'
+	page+='<html><head>'
+	page+='<title>software statistics</title>'
+	page+='</head>'
+	page+='<body>'
+	page+='<h1 align="center">software statistics</h1>'
+	page+='<form name="input" action="/" method="post" align="center">'
+	page+='First :<input type="text" name="catalogue0">'
+	page+='Second :<input type="text" name="catalogue1">'
+	page+='Third :<input type="text" name="catalogue2">'
+	page+='<input type="submit" value="Submit">'
+	page+='</form>'
+	page+='</body>'
+	page+='</html>'
+	return page
+	
+def indexpage():
+	page='<!DOCTYPE HTML>'
+	page+='<html><head>'
+	page+='<title>software statistics</title>'
+	page+='</head>'
+	page+='<body>'
+	page+='<h1 align="center">software statistics</h1>'
+	page+='<form name="input" action="/" method="post" align="center">'
+	page+='<select name="first">'
+	page+='First :<option value="Shoes">Shoes</option>'
+	page+='</select>'
+	page+='<select name="second">'
+	page+='Second :<option value="Boys">Boys</option>'
+	page+='<option value="Girls">Girls</option>'
+	page+='</select>'
+	page+='<select name="third">'
+	page+='Third :<option value="Outdoor">Outdoor</option>'
+	page+='</select>'
+	page+='<input type="submit" value="Submit">'
+	page+='</form>'
+	page+='</body>'
+	page+='</html>'
+	return page
+
+def head():
+  head='<!DOCTYPE HTML>'
+  head+='<html><head>'
+  head+='<title>software statistics</title>'
+  head+='</head>'
+  return head
+  
+def body0(asin):
+	name = getname(asin)
+	information = getinformation(asin)
+	imgurl = getimgurl(asin)
+	css='<style type = "text/css" >'
+	css+='#asinimg {'
+	css+='width : 300px;'
+	css+='height : 400px;'
+	css+='}'
+	css+='}'
+	css+='</style>'
+
+	body='<body>'
+	body+='<h1 align="center">software statistics</h1>'
+	body+='<table cellpadding="10" cellspacing="0" border="0" align="center">'
+	body+='<tr>'
+	body+='<td id="asinimg"><img src="' + imgurl + '" /></td>'
+	body+='<td><table cellpadding="10" cellspacing="0" border="0" align="center">'
+	body+='<tr><td>'+name+'</td></tr>'
+	body+='<tr><td>'+information+'</td></tr>'
+	body+='</table></td>'
+	body+='</tr>'
+	body+='</table>'
+	body+='</body>'
+	return css+body
+  
+def body(content='nothing'):
+        print content
+        body='<body>' +content+'</body>'
+        return body
+  
+def foot():
+	foot='</html>'
+	return foot
+	
+def getimgurl(asin='B003YUC4YI'):
+	data = get_commodity_data(asin)
+	imgurl = data['productInfo'][0]['img']
+	return imgurl
+	
+def getinformation(asin='B003YUC4YI'):
+	data = get_commodity_data(asin)
+	information = data['productInfo'][0]['productDescription']
+	return information
+	
+def getname(asin='B003YUC4YI'):
+	data = get_commodity_data(asin)
+	print data['productInfo']
+	name = data['productInfo'][0]['name']
+	return name
---
+++ kakajxs/singleprice.py
@@ -0,0 +1,77 @@
+#-*- coding:utf-8 -*-
+
+import urllib
+import simplejson as json
+import matplotlib.pyplot as plt
+from datetime import datetime
+from data import get_commodity_data
+
+def price(asin):
+
+        data = get_commodity_data(asin)
+        publishTime_list=[]
+
+        for every in data['review']:
+                publishTime_list.append(every['publishTime'])
+                
+        publishTimeMonth_list=[0 for i in range(12)]
+
+        for every in publishTime_list:
+                if(int(datetime.strptime(every, 
+                                           '%Y-%m-%d %H:%M:%S').strftime('%Y'))==2013):
+                        publishTimeMonth_list[int(datetime.strptime(every, 
+                                           '%Y-%m-%d %H:%M:%S').strftime('%m'))-1]+=1
+
+
+        mouth= [1,2,3,4,5,6,7,8,9,10,11,12];                      
+        str_publishTimeMonth_list = ",".join(str(item) for item in publishTimeMonth_list)
+        str_month_list = "','".join(str(item) for item in mouth)
+        result=str_month_list+'/'+str_publishTimeMonth_list
+        return result
+
+def highchart(asin):
+        result = price(asin)
+        str_month_list=result.split('/')[0]
+        str_publishTimeMonth_list=result.split('/')[1]
+        #print str_date
+        #print str_price
+        content='''<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>'''
+        content+='''<script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>'''
+        content+='''<script>'''
+        content+='''$(function () {'''
+        content+='''$('#container').highcharts({'''
+        content+='''title: {'''
+        content+='''text: 'Price Trend','''
+        content+='''x: -20'''
+        content+='''},'''
+        content+='''xAxis: {'''
+        content+='''categories: [' '''+str_month_list+''' ']'''
+        content+='''},'''
+        content+='''yAxis: {'''
+        content+='''title: {'''
+        content+='''text:'Price' '''
+        content+='''},'''
+        content+='''},'''
+        content+='''legend: {'''
+        content+='''layout: 'vertical','''
+        content+='''align: 'right','''
+        content+='''verticalAlign: 'middle','''
+        content+='''borderWidth: 0'''
+        content+='''},'''
+        content+='''series: [{'''
+        content+='''data: ['''+str_publishTimeMonth_list+''' ]'''
+        content+='''}]'''
+        content+='''});'''
+        content+='''});'''
+        content+='''</script>'''
+        content+='''<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>'''
+        return content
+
+def show(price_list,date_list):
+        
+        plt.plot(date_list, price_list, 'o-')
+        plt.gcf().autofmt_xdate()   #自动调整日期显示的格式
+        plt.xlabel('date')
+        plt.ylabel('price')
+    
+        plt.show()
