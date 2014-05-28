cb35269f38e9432004aedbd7b683923e894c84c3
kakajxs <jiangxusheng@114-212-43-242.nju.edu.cn>
2014-05-28 20:51:09 +0800

    星级和评论数

--- kakajxs/html.py
+++ kakajxs/html.py
@@ -1,5 +1,7 @@
 from data import get_commodity_data
 
+table = []
+
 def indexpage0():
 	page='<!DOCTYPE HTML>'
 	page+='<html><head>'
@@ -25,15 +27,18 @@ def indexpage():
 	page+='<body>'
 	page+='<h1 align="center">software statistics</h1>'
 	page+='<form name="input" action="/" method="post" align="center">'
-	page+='<select name="first">'
-	page+='First :<option value="Shoes">Shoes</option>'
+	page+='<label for="first">First :  </label>'
+	page+='<select name="first" id="first">'
+	page+='<option value="Shoes">Shoes</option>'
 	page+='</select>'
-	page+='<select name="second">'
-	page+='Second :<option value="Boys">Boys</option>'
+	page+='<label for="second"> Second :  </label>'
+	page+='<select name="second" id="second">'
+	page+='<option value="Boys">Boys</option>'
 	page+='<option value="Girls">Girls</option>'
 	page+='</select>'
-	page+='<select name="third">'
-	page+='Third :<option value="Outdoor">Outdoor</option>'
+	page+='<label for="third"> Third :  </label>'
+	page+='<select name="third" id="third" >'
+	page+='<option value="Outdoor">Outdoor</option>'
 	page+='</select>'
 	page+='<input type="submit" value="Submit">'
 	page+='</form>'
@@ -52,11 +57,17 @@ def body0(asin):
 	name = getname(asin)
 	information = getinformation(asin)
 	imgurl = getimgurl(asin)
+
 	css='<style type = "text/css" >'
-	css+='#asinimg {'
-	css+='width : 300px;'
-	css+='height : 400px;'
+	css+='#informations a:hover{'
+	css+='background-color:yellow;'
 	css+='}'
+	css+='#informations a:link{'
+	css+='text-decoration:none;'
+	css+='}'
+	css+='.tables{'
+	css+='height:300px;'
+	css+='width:750px;'
 	css+='}'
 	css+='</style>'
 
@@ -64,20 +75,34 @@ def body0(asin):
 	body+='<h1 align="center">software statistics</h1>'
 	body+='<table cellpadding="10" cellspacing="0" border="0" align="center">'
 	body+='<tr>'
-	body+='<td id="asinimg"><img src="' + imgurl + '" /></td>'
-	body+='<td><table cellpadding="10" cellspacing="0" border="0" align="center">'
-	body+='<tr><td>'+name+'</td></tr>'
+	body+='<table border="0" align="center">'
+	body+='<tr>'
+	body+='<td><img src="' + imgurl + '" /></td>'
+	body+='<td><table id="informations" cellpadding="10" cellspacing="0" border="0" align="center">'
+	body+='<tr><td><a href="http://www.amazon.com/dp/'+asin+'"><h3>'+name+'</h3></a></td></tr>'
 	body+='<tr><td>'+information+'</td></tr>'
 	body+='</table></td>'
 	body+='</tr>'
 	body+='</table>'
+	body+='</tr>'
+	for each in table:
+		body+='<tr>'
+		body+='<td>'
+		body+='<div class="tables">'
+		body+=each
+		body+='</div>'
+		body+='</td>'
+		body+='</tr>'
+	body+='</table>'
 	body+='</body>'
 	return css+body
+	
+def addtable(table0):
+	table.append(table0)
   
 def body(content='nothing'):
-        print content
-        body='<body>' +content+'</body>'
-        return body
+	body='<body>' +content+'</body>'
+	return body
   
 def foot():
 	foot='</html>'
