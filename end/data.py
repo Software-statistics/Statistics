234954f1fe551f83da11704b48f4f3771d0b4e3b
haha1994 <feng@haha.local>
2014-05-30 15:15:33 +0800

    更新end文件夹下的data.py，添加get_category_page方法

--- end/data.py
+++ end/data.py
@@ -3,33 +3,26 @@ import urllib
 import simplejson as json
 
 REQUEST_URL = 'http://112.124.1.3:8004'
+all_c = 'api/commodity/'
 
 # 获取sort分类下第n个数据所在页
 def get_category_data(sort, n):
-        all_c = 'api/commodity/'
-        
         a = int(n/20)
         data = json.loads(urllib.urlopen('?'.join([('/'.join([REQUEST_URL,all_c])),urllib.urlencode({'category_name':sort ,'page':a})])).read())
         return data
         
 # 获取特定商品编码数据			
 def get_commodity_data(asin):
-        all_c = 'api/commodity/'
-       
         data = json.loads(urllib.urlopen(''.join([('/'.join([REQUEST_URL,all_c])),asin])).read())
         return data
         
 # 得到sort分类下商品的数量
 def get_category_count(sort):
-        all_c = 'api/commodity/count/'
-       
         data = json.loads(urllib.urlopen('?'.join([('/'.join([REQUEST_URL,all_c])),urllib.urlencode({'category_name':sort})])).read())
         return data['count']
 
 # 得到sort分类下所有数据
 def get_category_all(sort):
-        all_c = 'api/commodity/'
-        
         n = get_category_count(sort)
         
         data = []
@@ -41,7 +34,10 @@ def get_category_all(sort):
 
 # 得到sort分类下field下的数据（第一页）
 def get_commodity_field(sort,field):
-        all_c = 'api/commodity/'
-
         data = json.loads(urllib.urlopen('?'.join([('/'.join([REQUEST_URL,all_c])),urllib.urlencode({'category_name':sort ,'field':field})])).read())
+        return data
+
+# 得到sort分类下第n页的数据
+def get_category_page(sort,page):
+				data = json.loads(urllib.urlopen('?'.join([('/'.join([REQUEST_URL,all_c])),urllib.urlencode({'category_name':sort ,'page':page})])).read())
         return data
