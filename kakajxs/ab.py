22baeea40ed82b5f817b5b4d540490607ca5b9b5
kakajxs <jiangxusheng@114-212-43-242.nju.edu.cn>
2014-06-03 20:11:03 +0800

    分类下星级统计

--- kakajxs/ab.py
+++ kakajxs/ab.py
@@ -47,7 +47,7 @@ while True:
             pass
         html.addtable(star.highchart('B003YUC4YI'))
         #html.addtable(singlecomment.highchart('B003YUC4YI'))
-        content = html.head() + html.body0('B003YUC4YI') + html.foot()
+        #content = html.head() + html.body0('B003YUC4YI') + html.foot()
 
         value = entry[-1].split('=')[-1]
         
